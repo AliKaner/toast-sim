be4794d3cd940dd0f82965a28d081fd272d0e012
Ali Kaner <alikaner.dev@gmail.com>
2026-01-27 14:06:00 +0300

    feat: Initialize Vue 3 project with Vite, configure TypeScript, and integrate Jest for testing.

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.ts$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json'
+    }]
+  },
+  moduleFileExtensions: ['vue', 'js', 'ts', 'json'],
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// Vue 3 + TypeScript starter
+</script>
+
+<template>
+  <div id="app">
+    <!-- App content will be added here -->
+  </div>
+</template>
+
+<style lang="scss">
+@import './styles/main.scss';
+</style>
