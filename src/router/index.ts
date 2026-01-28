39e1b49bc70886ba2c6d48fb2aa695e7a7734821
Ali Kaner <alikaner.dev@gmail.com>
2026-01-29 02:29:39 +0300

    update: updated router for deleted DemoPage

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/Home.vue'
-import DemoPage from '../pages/Demo.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage,
-    },
-    {
-      path: '/demo',
-      name: 'demo',
-      component: DemoPage,
     },
   ],
 })
