f62347659335ce4310b46a19f65c004eae7f72ff
Ali Kaner <alikaner.dev@gmail.com>
2026-01-29 03:31:56 +0300

    feat:  added spinner component

---
+++ src/components/ui/Spinner.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+interface SpinnerProps {
+  size?: 'small' | 'default' | 'large'
+}
+
+withDefaults(defineProps<SpinnerProps>(), {
+  size: 'default'
+})
+</script>
+
+<template>
+  <span class="spinner" :class="`spinner--${size}`"></span>
+</template>
+
+<style scoped>
+.spinner {
+  display: inline-block;
+  border: 0.125rem solid currentColor;
+  border-top-color: transparent;
+  border-radius: 50%;
+  animation: spin 0.5s linear infinite;
+}
+
+.spinner--small {
+  width: 0.75rem;
+  height: 0.75rem;
+}
+
+.spinner--default {
+  width: 1rem;
+  height: 1rem;
+}
+
+.spinner--large {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
